3ca9c18ca2a5337150cbdcb16f7ee61c07d10c1e
Chris Malven <cmalven@chrismalven.com>
2015-06-20 17:05:04 -0500

    Work in progress

--- Swift Bugs/GameScene.swift
+++ Swift Bugs/GameScene.swift
@@ -9,33 +9,42 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
+        /* Setup your scene here
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
+        */
+        
+        for idx in 1...20 {
+            let mySprite = SKSpriteNode(imageNamed:"bug")
+            let frameX = arc4random_uniform(UInt32(UInt(self.frame.width)))
+            let frameY = arc4random_uniform(UInt32(UInt(self.frame.height)))
+            mySprite.position = CGPoint(x:CGFloat(frameX), y:CGFloat(frameY))
+            mySprite.xScale = 0.5
+            mySprite.yScale = 0.5
+            self.addChild(mySprite)
+        }
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            for sprite in self.children {
+                
+                let frameX = arc4random_uniform(UInt32(UInt(self.frame.width)))
+                let frameY = arc4random_uniform(UInt32(UInt(self.frame.height)))
+                
+                let action = SKAction.moveTo(CGPoint(x:CGFloat(frameX), y:CGFloat(frameY)), duration: 2)
+                sprite.runAction(action)
+            }
         }
     }
    
